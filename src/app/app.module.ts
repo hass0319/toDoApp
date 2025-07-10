60ede07228bdd6359dfd8852fdabaea9379b6f60
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2025-07-10 13:23:39 +0300

    edited logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './auth.interceptor';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { EditTaskComponent } from './edit-task/edit-task.component';
-import { FilterTasksComponent } from './filter-tasks/filter-tasks.component';
-import { LoginComponent } from './login/login.component';
 import { TodoPageComponent } from './todo-page/todo-page.component';
+import { FilterTasksComponent } from './filter-tasks/filter-tasks.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { TodoPageComponent } from './todo-page/todo-page.component';
   providers: [
     {provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi:true}
+      multi:true},
+      { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent],
 })
